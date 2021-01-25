c7ea7dd843fd0959481a57b9bd2a63b0b4ef54f5
junior <morabav@gmail.com>
2021-01-25 21:32:46 +0200

    node modules added and banner

---
+++ src/components/banner.js
@@ -0,0 +1,99 @@
+import React from "react"
+import { Link, useStaticQuery } from "gatsby"
+import {Grid, Hidden,makeStyles, Typography } from "@material-ui/core"
+import BackgroundImage from "gatsby-background-image"
+import {graphql} from "gatsby"
+import Lottie from 'lottie-react-web';
+import welcomeLottie from '../animations/finalDraft.json'
+
+
+const useStyles = makeStyles(() => ({
+    masthead:{
+        width: '100%',
+        height: '90vh',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+    },
+    blackOverlayLottie:{
+        display: '-webkit-flex', //safari
+        display: 'flex',
+        backgroundColor: 'rgba(0,0,0,0.5',
+        height: '100%',
+        alignItems: 'center',
+        color: 'white',
+        justifyContent: 'flex-start'
+    },
+    blackOverlaytext:{
+        display: 'flex',
+        backgroundColor: 'rgba(0,0,0,0.5',
+        height: '100%',
+        alignItems: 'center',
+        color: 'white',
+    },
+    contentBox:{
+        margin: '0 auto',
+        maxWidth: '700px',
+        
+    },
+
+}));
+
+
+export default function Banner (){
+    const classes = useStyles();
+    const defaultOptions={
+        loop: false,
+        autoplay: true,
+        animationData: welcomeLottie,
+        renderer: 'svg',
+    };
+    const data = useStaticQuery(
+        graphql`
+            query{
+                seriousImage: file(relativePath: {eq: "serious1.jpg"}){
+                    childImageSharp{
+                        fluid(maxWidth: 900){
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+                largeScreen: file(relativePath: {eq: "serious2.jpg"}){
+                    childImageSharp{
+                        fluid(maxWidth: 1800, quality: 100){
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        `
+    )
+    
+    return (
+        <Grid container>
+            <Hidden smDown>
+                <Grid item xs={12} >
+                    <BackgroundImage fluid={data.largeScreen.childImageSharp.fluid} className={classes.masthead}>
+                            <div className={classes.blackOverlayLottie}>
+                                <div className={classes.contentBox}>
+                                    <Lottie  options={defaultOptions} />
+                                </div>
+                            </div>
+                       
+                    </BackgroundImage>
+                </Grid>
+            </Hidden>
+
+            <Hidden smUp>
+                <Grid item xs={12}>
+                    <BackgroundImage fluid={data.seriousImage.childImageSharp.fluid} className={classes.masthead}>
+                        <div>
+                            <Typography variant='h3' style={{position:'absolute',left:'50%',bottom:0,transform:'translate(-50%,0%)',msTransform:'translate(-50%,0%)',textAlign:'center',color:'white'}}>Hey I"m<br/>Junior Moraba</Typography>
+                        </div>
+                    </BackgroundImage>
+                </Grid>
+            </Hidden>
+            
+        </Grid>
+    )
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,190 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  IconButton,
+  Drawer,
+  Link,
+  MenuItem,
+  Avatar,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const headersData = [
+  {
+    label: "HOME",
+    href: "#home",
+  },
+  {
+    label: "ABOUT",
+    href: "#about",
+  },
+  {
+    label: "MY SKILLS",
+    href: "#myskills",
+  },
+  {
+    label: "MY Reputation",
+    href: "#myreputation",
+  },
+  {
+    label: "HIRE ME",
+    href: "#hireme",
+  },
+  {
+    label: "CONTACT",
+    href: "#contact",
+  },
+];
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+const useStyles = makeStyles(() => ({
+  header: {
+    //backgroundColor: "#fff",
+    background: "linear-gradient(to right, rgba(0,0,0), rgba(3,147,218,1))",
+    paddingRight: "79px",
+    paddingLeft: "118px",
+    "@media (max-width: 900px)": {
+      paddingLeft: 0,
+    },
+  },
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#fff",
+    textAlign: "left",
+  },
+  menuButton: {
+    fontFamily: "Open Sans, sans-serif",
+    fontWeight: 700,
+    size: "18px",
+    marginLeft: "38px",
+    color: "#fff",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  drawerContainer: {
+    padding: "20px 30px",
+  },
+}));
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+export default function Header() {
+    const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
 
-export default Header
+    const [state, setState] = useState({
+      mobileView: false,
+      drawerOpen: false,
+    });
+
+    const { mobileView, drawerOpen } = state;
+
+    useEffect(() => {
+      const setResponsiveness = () => {
+        return window.innerWidth < 900
+          ? setState((prevState) => ({ ...prevState, mobileView: true }))
+          : setState((prevState) => ({ ...prevState, mobileView: false }));
+      };
+
+      setResponsiveness();
+
+      window.addEventListener("resize", () => setResponsiveness());
+    }, []);
+
+    const displayDesktop = () => {
+      return (
+        <Toolbar className={toolbar}>
+          {EandRLogo}
+          <div>{getMenuButtons()}</div>
+        </Toolbar>
+      );
+    };
+
+    const displayMobile = () => {
+      const handleDrawerOpen = () =>
+        setState((prevState) => ({ ...prevState, drawerOpen: true }));
+      const handleDrawerClose = () =>
+        setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+      return (
+        <Toolbar>
+          <IconButton
+            {...{
+              edge: "start",
+              color: "inherit",
+              "aria-label": "menu",
+              "aria-haspopup": "true",
+              onClick: handleDrawerOpen,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+  
+          <Drawer
+            {...{
+              anchor: "left",
+              open: drawerOpen,
+              onClose: handleDrawerClose,
+            }}
+          >
+            <div className={drawerContainer}>{getDrawerChoices()}</div>
+          </Drawer>
+  
+          <div>{EandRLogo}</div>
+        </Toolbar>
+      );
+    };
+    
+    const getDrawerChoices = () => {
+      return headersData.map(({ label, href }) => {
+        return (
+            <Link
+              {...{
+                component: RouterLink,
+                to: href,
+                color: "inherit",
+                style: { textDecoration: "none" },
+                key: label,
+              }}
+            >
+              <MenuItem>{label}</MenuItem>
+            </Link>
+        );
+      });
+    };
+    const EandRLogo = (
+      <Link href='/'><Typography variant="h4" className={logo}> Einstein & Robin</Typography></Link>
+      
+    );
+    
+
+    const getMenuButtons = () => {
+      return headersData.map(({ label, href }) => {
+        return (
+          <Button
+            {...{
+              key: label,
+              color: "inherit",
+              to: href,
+              component: RouterLink,
+              className: menuButton,
+            }}
+          >
+            {label}
+          </Button>
+        );
+      });
+    };
+
+    return(
+      <header>
+        <AppBar className={header}>
+          {mobileView ? displayMobile() : displayDesktop()}
+        </AppBar>
+      </header> 
+    );
+  }
