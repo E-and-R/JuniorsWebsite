bf74913e469248bdbfc2632726e94761b9d388ca
junior <morabav@gmail.com>
2021-01-26 11:25:27 +0200

    removed skillbar

--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -5,7 +5,7 @@ import PhoneIcon from '@material-ui/icons/Phone';
 import EmailIcon from '@material-ui/icons/Email';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import SkillBar from 'react-skillbars';
+
 import Lottie from 'lottie-react-web';
 import aboutme from '../animations/aboutme.json'
 import contact from '../animations/contactComp.json'
@@ -123,7 +123,7 @@ export default function HomeContent(){
                     </Hidden>
                     <br/><br/>
                     <Grid item xs={11} md={10} style={{margin:'0 auto'}}>
-                        <SkillBar skills={skillbar} colors={colors}  />
+                        
                     </Grid>
                     
                 </Grid>
